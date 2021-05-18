3dc9cff1a3b1f385e8e39324f2a90af5a57fa46f
Carolinacapote <carolina.capote28@gmail.com>
2021-05-18 00:31:45 +0000

    Dividing numbers and printing errors

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,8 +12,9 @@ def list_division(my_list_1, my_list_2, list_length):
             division = 0
 
         except ZeroDivisionError:
-             print("division by 0")
-             division = 0
+            print("division by 0")
+            division = 0
+
         except IndexError:
             print("out of range")
             division = 0
