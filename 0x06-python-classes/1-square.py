16ee56750502dbc431d09b266cca03390e13040c
Carolinacapote <carolina.capote28@gmail.com>
2021-05-18 20:34:17 +0000

    Class with private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """Class that defines a square."""
+    def __init__(self, size = 0):
+        """__init__ initialitazion of a private instance attribute."""
+        self.__size = size
