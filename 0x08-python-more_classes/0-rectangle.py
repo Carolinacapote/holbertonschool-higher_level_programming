a945ffaa261edd07b47a20052c8066566f916197
Carolinacapote <carolina.capote28@gmail.com>
2021-05-20 17:00:35 +0000

    Empty class called Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains and empty class."""
+
+
+class Rectangle:
+    """Empty class called Rectangle, does not do anything and have no
+    instances or attributes"""
+    pass
