bb3030b6d4ed3992250111f0ccd8f498a1f0e432
Carolinacapote <carolina.capote28@gmail.com>
2021-06-02 22:31:54 +0000

    Adding a method to the class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module contains a class called
+BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    # Public instance method.
+    def area(self):
+        """
+        Exceptions:
+            msg: area() is not implemented.
+        """
+        raise Exception('area() is not implemented')
