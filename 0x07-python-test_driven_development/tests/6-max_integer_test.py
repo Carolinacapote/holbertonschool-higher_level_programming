6fd7f4c72b3280bdf7ee8a1e8a460401ca40a841
Carolinacapote <carolina.capote28@gmail.com>
2021-06-02 00:40:42 +0000

    Unittest for a function

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,19 +12,10 @@ class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
         # Test max_ integer when argument is a list of integers
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertEqual(max_integer([5, 1, 3, 2]), 5)
         self.assertEqual(max_integer([1, 3, -4, 2]), 3)
+        self.assertEqual(max_integer([1, 3, -4, 2, 5]), 5)
+        self.assertEqual(max_integer([-1, -3, -4, -2]), -1)
+        self.assertEqual(max_integer([3]), 3)
         self.assertEqual(max_integer(), None)
         self.assertEqual(max_integer([]), None)
-
-    def test_types(self):
-        # Make sure TypeErrors are raised when necessary
-        self.assertRaises(TypeError, max_integer, 3.5)
-        self.assertRaises(TypeError, max_integer, 'hello')
-        self.assertRaises(TypeError, max_integer, None)
-        self.assertRaises(TypeError, max_integer, [3, 'hello', 3.5])
-        self.assertRaises(TypeError, max_integer, 5)
-        self.assertRaises(TypeError, max_integer, True)
-        self.assertRaises(TypeError, max_integer, 3, 5)
-        self.assertRaises(TypeError, max_integer, {1, 2, 3})
-        self.assertRaises(TypeError, max_integer, {'value_1': 5, 'value_2': 6})
-        self.assertRaises(TypeError, max_integer, (1, 4, 3, 5))
