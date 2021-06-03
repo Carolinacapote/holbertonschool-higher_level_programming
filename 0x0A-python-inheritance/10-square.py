80d0dd5c849809ad10bb958b833b8a805348d8de
Carolinacapote <carolina.capote28@gmail.com>
2021-06-03 00:31:06 +0000

    New class Square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module contains a class called Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class that inherits from Rectangle (9-rectangle.py)"""
+    def __init__(self, size):
+        self.integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        """Area of the figure"""
+        return(self.__size ** 2)
+
+    def __str__(self):
+        """Magic attribute __str__ to return a string"""
+        return('[Rectangle] {}/{}'.format(self.__size, self.__size))
