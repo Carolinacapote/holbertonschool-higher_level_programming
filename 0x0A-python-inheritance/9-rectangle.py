a0f3a0e7cc167ea37ff7243729303e7e50e9c92e
Carolinacapote <carolina.capote28@gmail.com>
2021-06-03 00:21:50 +0000

    adding the method area()

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module contains a class called Rectangle"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class rectangle that inherits from BaseGeometry (7-base_geometry.py)"""
+    def __init__(self, width, height):
+        self.integer_validator('width', width)
+        self.__width = width
+        self.integer_validator('height', height)
+        self.__height = height
+
+    def area(self):
+        """Area of the figure"""
+        return(self.__width * self.__height)
+
+    def __str__(self):
+        """Magic attribute __str__ to return a string"""
+        return('[Rectangle] {}/{}'.format(self.__width, self.__height))
