e8c032e0a2a66aaa31cd1c93362a5df1fcbff9cb
Carolinacapote <carolina.capote28@gmail.com>
2021-06-06 22:35:07 +0000

    Class Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This module contains a superclass called *Base*"""
+import models
+import json
 
 
 class Base:
@@ -19,3 +21,47 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns:
+            The JSON string representation of 'list_dictionaries'
+            If 'list_dictionaries' is None or empty, return: []
+        """
+        if list_dictionaries is None or len(list_dictionaries) <= 0:
+            return '[]'
+
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """This function writes the JSON string representation of list_objs
+        to a file.
+        Arguments:
+            list_objs (list): list of instances who inherits of Base.
+        """
+        my_dictionary = {}
+        my_list = []
+        if list_objs is None:
+            with open('Rectangle.json', mode='w') as my_file:
+                my_file.write(my_list)
+
+        for obj in list_objs:
+            if type(obj) is models.square.Square:
+                my_dictionary = cls.to_dictionary(obj)
+                my_list.append(my_dictionary)
+        
+                with open('Square.json', mode='w') as my_file:
+                    my_file.write(cls.to_json_string(my_list))
+
+            if type(obj) is models.rectangle.Rectangle:
+                my_dictionary = cls.to_dictionary(obj)
+                my_list.append(my_dictionary)
+        
+                with open('Rectangle.json', mode='w') as my_file:
+                    my_file.write(cls.to_json_string(my_list))
+
+    @staticmethod
+    def from_json_string(json_string):  # static method
+        """"""
