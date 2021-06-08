c690cbcc240e75267f9f108c7cf14f2cf4e6f959
Carolinacapote <carolina.capote28@gmail.com>
2021-06-08 00:57:19 +0000

    Test file

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,6 +3,7 @@
 """
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 class TestBase(unittest.TestCase):
@@ -11,3 +12,18 @@ class TestBase(unittest.TestCase):
     """
     def test_base_class(self):
         """Function with some test cases"""
+        b1 = Base()
+        self.assertEqual(1, b1.id)
+        b2 = Base(12)
+        self.assertEqual(12, b2.id)
+        b3 = Base()
+        self.assertEqual(2, b3.id)
+
+    """def test_to_json_string(self):
+        # Testing to_json_string() function
+        r1 = Rectangle(10, 7, 2, 8, 3)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual(json_dictionary, '[{"id": 3, "width": 10,
+        "height": 7, "x": 2, "y": 8}]')
+        """
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -9,7 +9,7 @@ class TestSquare(unittest.TestCase):
     """Class that declares the test functions to test if the
     'Square' class is working correctly.
     """
-    def test_square_class(self):  #task 2
+    def test_square_class(self):
         """Function with some test cases"""
         pass
 
@@ -24,3 +24,39 @@ class TestSquare(unittest.TestCase):
         self.assertRaises(ValueError, Square, 0, 2, 3)
         self.assertRaises(ValueError, Square, 3, -3, 2)
         self.assertRaises(ValueError, Square, 2, 3, -2)
+
+    def test_area(self):
+        """Testing the area() function"""
+        s1 = Square(3)
+        self.assertEqual(9, s1.area())
+        s2 = Square(5, 0, 0, 12)
+        self.assertEqual(25, s2.area())
+
+    def test_update(self):
+        """Testing the update() function"""
+        s1 = Square(10, 10, 10)
+        s1.update(89)
+        self.assertEqual(89, s1.id)
+        s1.update(79, 2)
+        self.assertEqual(79, s1.id)
+        self.assertEqual(2, s1.size)
+        s1.update(89, 2, 4, 5)
+        self.assertEqual(89, s1.id)
+        self.assertEqual(2, s1.size)
+        self.assertEqual(4, s1.x)
+        self.assertEqual(5, s1.y)
+
+        # update with kwargs
+        s2 = Square(5, 5, 5, 5)
+        s2.update(id=1)
+        self.assertEqual(1, s2.id)
+        s2.update(x=1, y=3, size=4)
+        self.assertEqual(1, s2.x)
+        self.assertEqual(3, s2.y)
+        self.assertEqual(4, s2.size)
+
+    def test_to_dictionary(self):
+        """Testing to-dictionary() function"""
+        s1 = Square(10, 2, 1, 9)
+        s1_dictionary = s1.to_dictionary()
+        self.assertEqual(s1_dictionary, {'x': 2, 'y': 1, 'id': 9, 'size': 10})
