be4617b935b9b3f2a057590cd4fb16e1955c757e
Carolinacapote <carolina.capote28@gmail.com>
2021-06-08 22:41:46 +0000

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """This module contains a superclass called *Base*"""
 import json
+import csv
 from os import path
 
 
 class Base:
     """This class will be the "base" of the other classes (Rectangle, Square)
     """
-    __nb_objects = 0  # Private class attribute
+    __nb_my_objects = 0  # Private class attribute
 
     def __init__(self, id=None):
         """
         Conditionals:
             if "id" is not None, assign the "id" with this argument value.
-            Otherwise id equals to __nb_objects
+            Otherwise id equals to __nb_my_objects
         """
         if id is not None:
             self.id = id
 
         else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            Base.__nb_my_objects += 1
+            self.id = Base.__nb_my_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
@@ -93,3 +94,34 @@ class Base:
                     instances_list.append(cls.create(**dictionary))
 
         return instances_list
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Function that serializes in CSV, load from a CSV file"""
+        if not path.exists(cls.__name__ + '.csv'):
+            return []
+        if cls.__name__ == 'Rectangle':
+            attrs = ('id', 'width', 'height', 'x', 'y')
+        elif cls.__name__ == 'Square':
+            attrs = ('id', 'size', 'x', 'y')
+        with open(cls.__name__ + '.csv', 'rt', newline='') as my_file:
+            reader = csv.reader(my_file)
+            my_objects = list(reader)
+        my_objects = ((int(i) for i in l) for l in my_objects)
+        return [cls.create(**dict(zip(attrs, l))) for l in my_objects]
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Function that deserializes in CSV, save instances to a CSV file"""
+        if list_objs is None:
+            list_objs = []
+
+        if cls.__name__ == 'Rectangle':
+            attrs = ('id', 'width', 'height', 'x', 'y')
+        elif cls.__name__ == 'Square':
+            attrs = ('id', 'size', 'x', 'y')
+        list_objs = ([getattr(o, a) for a in attrs] for o in list_objs)
+        with open(cls.__name__ + '.csv', 'wt', newline='') as file:
+            writer = csv.writer(file)
+            for row in list_objs:
+                writer.writerow(row)
