3ef12f9d09db5da9418b0e5842df3ce9567bdcc2
Carolinacapote <carolina.capote28@gmail.com>
2021-07-13 23:15:02 +0000

    Create table with id equals 1

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 --  script that creates the table id_not_null on your MySQL server.
 -- If the table already exists, script does not fail.
-CREATE TABLE IF NOT EXISTS id_not_null (id INT(1),name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1,name VARCHAR(256));
