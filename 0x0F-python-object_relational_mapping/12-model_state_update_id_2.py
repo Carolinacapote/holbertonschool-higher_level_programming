b34a3a93224dae05dc767d3c493030e5bf72c24d
Carolinacapote <carolina.capote28@gmail.com>
2021-08-08 13:01:43 +0000

    [feature] change the name of a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+Script that changes the name of a State object from the database hbtn_0e_6_usa
+'''
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    user_name = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(user_name, passwd, db), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = 'New Mexico'
+
+    session.commit()
+    session.close()
