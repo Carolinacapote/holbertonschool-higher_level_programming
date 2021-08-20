2e83ace5d3876efab72486f023e701e7ab101d6d
Carolinacapote <carolina.capote28@gmail.com>
2021-08-20 22:41:10 +0000

    [FEATURE]Send a POST request and display body of the response

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Python script that takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter, and displays the body of the response
+(decoded in utf-8)
+'''
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    from sys import argv
+
+    url = argv[1]
+    parameters = {
+        'email': argv[2]
+    }
+
+    query_str = urllib.parse.urlencode(parameters)
+    data = query_str.encode('UTF-8')
+
+    with urllib.request.urlopen(url, data) as response:
+        response_text = response.read()
+    print(response_text)
