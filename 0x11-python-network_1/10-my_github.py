a67f9eead34483d02c37d62483535ce016afc896
Carolinacapote <carolina.capote28@gmail.com>
2021-08-21 03:56:19 +0000

    [FEATURE]Display user id of github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+Python script that takes your GitHub credentials (username and password) and
+uses the GitHub API to display your id.
+ Basic Authentication with a personal access token as password to access.
+'''
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = 'https://api.github.com/user'
+    user_name = argv[1]
+    passwd = argv[2]
+
+    response = requests.get(url, auth=(user_name, passwd))
+    user_id = response.json.get('id')
+
+    print(user_id)
