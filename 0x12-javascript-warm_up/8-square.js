5afd535d947b16148d74ba9de3e3b8cc8dc4b4e7
Carolinacapote <carolina.capote28@gmail.com>
2021-08-22 03:04:44 +0000

    [FEATURE]Print a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* Script that prints a square */
+
+const size = process.argv[2];
+
+if (Number(size)) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+} else {
+  console.log('Missing size');
+}
