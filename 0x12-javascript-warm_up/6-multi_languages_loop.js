c42190907ec3a8a0e4a76d396ca1e8cab12cca7a
Carolinacapote <carolina.capote28@gmail.com>
2021-08-22 02:26:49 +0000

    [FEATURE]Print 3 lines with loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* Script that prints 3 lines using an array of string and a loop */
+
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
