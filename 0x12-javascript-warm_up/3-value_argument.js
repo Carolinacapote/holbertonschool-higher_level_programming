e65dce6d9fc851499e125d28e45c2a28079e19fd
Carolinacapote <carolina.capote28@gmail.com>
2021-08-22 01:37:39 +0000

    [FIX]Delete length method

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 /* Script that prints the first argument passed to it */
 
-if (process.argv.length === 2) {
-  console.log('No argument');
-} else {
+if (process.argv[2]) {
   console.log(process.argv[2]);
+} else {
+  console.log('No argument');
 }
